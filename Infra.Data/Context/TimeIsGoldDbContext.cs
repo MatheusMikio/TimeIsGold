930551b24d0e999a93c0263ceec91bf497e3f53c
Matheus Mikio <matheusmikio15@gmail.com>
2025-11-12 21:49:09 -0300

    Mudança de versão dos pacotes

--- Infra.Data/Context/TimeIsGoldDbContext.cs
+++ Infra.Data/Context/TimeIsGoldDbContext.cs
@@ -34,16 +34,16 @@ namespace Infrastructure.Data
                 .WithOne(e => e.Plan)
                 .HasForeignKey(e => e.PlanId);
 
-            // Marcar Address como tipo complexo (owned)
-            //modelBuilder.Entity<Enterprise>()
-            //    .OwnsOne(e => e.Address, address =>
-            //    {
-            //        address.Property(a => a.Street).HasColumnName("Address_Street");
-            //        address.Property(a => a.Number).HasColumnName("Address_Number");
-            //        address.Property(a => a.City).HasColumnName("Address_City");
-            //        address.Property(a => a.State).HasColumnName("Address_State");
-            //        address.Property(a => a.Country).HasColumnName("Address_Country");
-            //    });
+            //Marcar Address como tipo complexo(owned)
+            modelBuilder.Entity<Enterprise>()
+                .OwnsOne(e => e.Address, address =>
+                {
+                    address.Property(a => a.Street).HasColumnName("Address_Street");
+                    address.Property(a => a.Number).HasColumnName("Address_Number");
+                    address.Property(a => a.City).HasColumnName("Address_City");
+                    address.Property(a => a.State).HasColumnName("Address_State");
+                    address.Property(a => a.Country).HasColumnName("Address_Country");
+                });
 
             // Adicionando relacionamentos para Scheduling
             modelBuilder.Entity<Scheduling>()
